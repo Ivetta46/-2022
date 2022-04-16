742e453db1e37120e70029d5d8e8bc7b20873525
Ivetta46 <79827292+Ivetta46@users.noreply.github.com>
2022-04-16 11:34:26 +0300

    задача 1

--- ДЗ6/main.cpp
+++ ДЗ6/main.cpp
@@ -1,94 +1,64 @@
-#include <queue>
-#include <mutex>
-#include <exception>
-#include <thread>
 #include <iostream>
+#include <numeric>
+#include <vector>
+#include <thread>
+#include <mutex>
 
 
-template<typename T>
-class ThreadsafeQueue {
-    std::queue<T> queue_;
-    mutable std::mutex mutex_;
-public:
-    ThreadsafeQueue() = default;
-    ThreadsafeQueue(const ThreadsafeQueue<T>&) = delete;
-    ThreadsafeQueue& operator=(const ThreadsafeQueue<T>&) = delete;
+std::mutex mtx;
 
-    ThreadsafeQueue(ThreadsafeQueue<T>&& other) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        queue_ = std::move(other.queue_);
+template <typename Iterator, typename T>
+void accumulate_wrapper(Iterator begin, Iterator end, T & result) {
+    T sum = std::accumulate(begin, end, 0);
+    mtx.lock();
+    result += sum;
+    mtx.unlock();
+}
+
+template <typename Iterator, typename T>
+void parallel_accumulate(Iterator begin, Iterator end, T & result) {
+    result = 0;
+    // Размер диапазона
+    auto size = std::distance(begin, end);
+    // Количество исполнителей
+    auto num_workers = std::thread::hardware_concurrency();
+    // Проверяем, нужно ли распараллеливать
+    if (size < num_workers * 4) {
+        result = std::accumulate(begin, end, 0);
+        return;
     }
-
-    virtual ~ThreadsafeQueue() { }
-
-    bool empty() const {
-        return queue_.empty();
+    // Распараллеливать всё-таки нужно, тогда:
+    // Считаем количество данных на одного исполнителя
+    auto size_per_worker = size / num_workers;
+    // Создаём два вектора: с потоками и для записи результатов
+    std::vector<std::thread> threads;
+    // Распределяем данные и запускаем потоки.
+    for(auto i = 0u; i < num_workers - 1; i++) {
+        threads.push_back(std::thread(accumulate_wrapper<Iterator, T>,
+                                      std::next(begin, i * size_per_worker), // сдвиг begin
+                                      std::next(begin, (i + 1) * size_per_worker), // сдвиг begin
+                                      std::ref(result))); // для записи результата
     }
+    // Производим расчёт и в основном потоке остатка последовательности
+    auto sum = std::accumulate(
+            std::next(begin, (num_workers - 1) * size_per_worker),
+            end, 0);
 
-    unsigned long size() const {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.size();
+    mtx.lock();
+    result += sum;
+    mtx.unlock();
+
+    // Ждём, пока остальные потоки завершат работу
+    for(auto & thread : threads) {
+        thread.join();
     }
+}
 
-    //убираем последний элемент
-    void pop() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (empty()) {
-            return;
-        }
-        queue_.pop();
-    }
-
-    //добавляем элемент в очередь
-    void push(const T& item) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        queue_.push(item);
-    }
-
-    //возвращаем первый элемент очереди
-    T front()
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.empty()) {
-            throw std::logic_error("No elements in queue");
-        }
-        return queue_.front();
-    }
-
-    //возвращаем последний элемент очереди
-    T back()
-    {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.empty()) {
-            throw std::logic_error("No elements in queue");
-        }
-        return queue_.back();
-    }
-};
-
-int main()
-{
-    ThreadsafeQueue<int> queue;
-    auto push_back = [&queue]()
-    {
-        for (int i = 0; i < 10; ++i)
-        {
-            queue.push(i);
-        }
-    };
-    std::thread t1(push_back); // поток для записи в очередь
-    auto pop = [&queue]()
-    {
-        for (int i = 0; i < 10; ++i)
-        {
-            queue.pop();
-        }
-    };
-    std::thread t2(pop); // поток для удаления элементов из очереди
-    t1.join();
-    t2.join();
-    if (queue.empty())
-        std::cout << "Everything is OK!";
-    else std::cout << "Some problems";
-    return 0;
+int main() {
+    std::vector<int> sequence(100);
+    //std::vector<int> sequence(100000);
+    std::iota(std::begin(sequence), std::end(sequence), 0);
+    long int result;
+    parallel_accumulate( std::begin(sequence), std::end(sequence), result );
+    std::cout << result << std::endl;
 }
